c2f9f305c321763966569ece83ee7b7b32e41eb6
Linn Erle <linnerlek@gmail.com>
2024-11-22 21:19:37 -0500

    Initial Commit

---
+++ PantherPath/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PantherPath
+//
+//  Created by Linn on 11/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PantherPath/PantherPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantherPathApp.swift
+//  PantherPath
+//
+//  Created by Linn on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct PantherPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
